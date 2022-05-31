feee10116f2c6bc4849045de649fb8eaa8cacbf8
julitis <julia.g.hernandez@gmail.com>
2022-05-31 12:04:19 +0200

    He añadido la vista 3

---
+++ JuliaGonzalez34/Form3.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace JuliaGonzalez34
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Productos3Lbl = new System.Windows.Forms.Label();
+            this.Nombre3Lbl = new System.Windows.Forms.Label();
+            this.Descripción3Lbl = new System.Windows.Forms.Label();
+            this.Categoría3Lbl = new System.Windows.Forms.Label();
+            this.Precio3Lbl = new System.Windows.Forms.Label();
+            this.Añadir3Btn = new System.Windows.Forms.Button();
+            this.Cancelar3Btn = new System.Windows.Forms.Button();
+            this.OtroProducto3Btn = new System.Windows.Forms.Button();
+            this.Finalizar3Btn = new System.Windows.Forms.Button();
+            this.DevuelveTotalBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Productos3Lbl
+            // 
+            this.Productos3Lbl.AutoSize = true;
+            this.Productos3Lbl.Location = new System.Drawing.Point(50, 62);
+            this.Productos3Lbl.Name = "Productos3Lbl";
+            this.Productos3Lbl.Size = new System.Drawing.Size(75, 20);
+            this.Productos3Lbl.TabIndex = 0;
+            this.Productos3Lbl.Text = "Productos";
+            // 
+            // Nombre3Lbl
+            // 
+            this.Nombre3Lbl.AutoSize = true;
+            this.Nombre3Lbl.Location = new System.Drawing.Point(226, 71);
+            this.Nombre3Lbl.Name = "Nombre3Lbl";
+            this.Nombre3Lbl.Size = new System.Drawing.Size(64, 20);
+            this.Nombre3Lbl.TabIndex = 1;
+            this.Nombre3Lbl.Text = "Nombre";
+            // 
+            // Descripción3Lbl
+            // 
+            this.Descripción3Lbl.AutoSize = true;
+            this.Descripción3Lbl.Location = new System.Drawing.Point(226, 120);
+            this.Descripción3Lbl.Name = "Descripción3Lbl";
+            this.Descripción3Lbl.Size = new System.Drawing.Size(87, 20);
+            this.Descripción3Lbl.TabIndex = 2;
+            this.Descripción3Lbl.Text = "Descripción";
+            // 
+            // Categoría3Lbl
+            // 
+            this.Categoría3Lbl.AutoSize = true;
+            this.Categoría3Lbl.Location = new System.Drawing.Point(226, 172);
+            this.Categoría3Lbl.Name = "Categoría3Lbl";
+            this.Categoría3Lbl.Size = new System.Drawing.Size(74, 20);
+            this.Categoría3Lbl.TabIndex = 3;
+            this.Categoría3Lbl.Text = "Categoria";
+            // 
+            // Precio3Lbl
+            // 
+            this.Precio3Lbl.AutoSize = true;
+            this.Precio3Lbl.Location = new System.Drawing.Point(226, 223);
+            this.Precio3Lbl.Name = "Precio3Lbl";
+            this.Precio3Lbl.Size = new System.Drawing.Size(50, 20);
+            this.Precio3Lbl.TabIndex = 4;
+            this.Precio3Lbl.Text = "Precio";
+            // 
+            // Añadir3Btn
+            // 
+            this.Añadir3Btn.Location = new System.Drawing.Point(503, 67);
+            this.Añadir3Btn.Name = "Añadir3Btn";
+            this.Añadir3Btn.Size = new System.Drawing.Size(94, 29);
+            this.Añadir3Btn.TabIndex = 5;
+            this.Añadir3Btn.Text = "Añadir";
+            this.Añadir3Btn.UseVisualStyleBackColor = true;
+            // 
+            // Cancelar3Btn
+            // 
+            this.Cancelar3Btn.Location = new System.Drawing.Point(62, 344);
+            this.Cancelar3Btn.Name = "Cancelar3Btn";
+            this.Cancelar3Btn.Size = new System.Drawing.Size(94, 29);
+            this.Cancelar3Btn.TabIndex = 6;
+            this.Cancelar3Btn.Text = "Cancelar";
+            this.Cancelar3Btn.UseVisualStyleBackColor = true;
+            // 
+            // OtroProducto3Btn
+            // 
+            this.OtroProducto3Btn.Location = new System.Drawing.Point(317, 348);
+            this.OtroProducto3Btn.Name = "OtroProducto3Btn";
+            this.OtroProducto3Btn.Size = new System.Drawing.Size(114, 29);
+            this.OtroProducto3Btn.TabIndex = 7;
+            this.OtroProducto3Btn.Text = "Otro Producto";
+            this.OtroProducto3Btn.UseVisualStyleBackColor = true;
+            // 
+            // Finalizar3Btn
+            // 
+            this.Finalizar3Btn.Location = new System.Drawing.Point(548, 354);
+            this.Finalizar3Btn.Name = "Finalizar3Btn";
+            this.Finalizar3Btn.Size = new System.Drawing.Size(94, 29);
+            this.Finalizar3Btn.TabIndex = 8;
+            this.Finalizar3Btn.Text = "Finalizar";
+            this.Finalizar3Btn.UseVisualStyleBackColor = true;
+            // 
+            // DevuelveTotalBtn
+            // 
+            this.DevuelveTotalBtn.Location = new System.Drawing.Point(179, 279);
+            this.DevuelveTotalBtn.Name = "DevuelveTotalBtn";
+            this.DevuelveTotalBtn.Size = new System.Drawing.Size(141, 29);
+            this.DevuelveTotalBtn.TabIndex = 9;
+            this.DevuelveTotalBtn.Text = "Devuelve el total del precio";
+            this.DevuelveTotalBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DevuelveTotalBtn);
+            this.Controls.Add(this.Finalizar3Btn);
+            this.Controls.Add(this.OtroProducto3Btn);
+            this.Controls.Add(this.Cancelar3Btn);
+            this.Controls.Add(this.Añadir3Btn);
+            this.Controls.Add(this.Precio3Lbl);
+            this.Controls.Add(this.Categoría3Lbl);
+            this.Controls.Add(this.Descripción3Lbl);
+            this.Controls.Add(this.Nombre3Lbl);
+            this.Controls.Add(this.Productos3Lbl);
+            this.Name = "Form3";
+            this.Text = "Hola soy el TPV rey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label Productos3Lbl;
+        private Label Nombre3Lbl;
+        private Label Descripción3Lbl;
+        private Label Categoría3Lbl;
+        private Label Precio3Lbl;
+        private Button Añadir3Btn;
+        private Button Cancelar3Btn;
+        private Button OtroProducto3Btn;
+        private Button Finalizar3Btn;
+        private Button DevuelveTotalBtn;
+    }
+}

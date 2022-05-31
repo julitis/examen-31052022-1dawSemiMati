f3857c9abe2040fd36514adb32b9defe3f7bb69a
julitis <julia.g.hernandez@gmail.com>
2022-05-31 12:46:41 +0200

    Hemos añadido la clase ProductModel

---
+++ JuliaGonzalez34/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliaGonzalez34
+{
+    public class ProductModel
+    {
+            public ProductModel(string name, string description, string category, int price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Category = category;
+            this.Price = price;
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+
+    }
+}
